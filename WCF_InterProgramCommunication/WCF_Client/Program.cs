fadc2544c1b55ffc21b09b24cddbd39ec76be7be
Ashton Stockman <ashton.stockman@live.com>
2016-03-01 20:49:57 -0600

    base functionality working

---
+++ WCF_InterProgramCommunication/WCF_Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF_Client
+{
+    [ServiceContract]
+    public interface IStringReverser
+    {
+        [OperationContract]
+        string ReverseString(string value);
+    }
+
+    class Program
+    {
+        public static string hostName = ConfigurationManager.AppSettings["hostName"].ToString();
+
+        static void Main(string[] args)
+        {
+            var ep = $"net.tcp://{hostName}:9985/TcpReverse";
+
+            var tcpBinding = new NetTcpBinding();
+            tcpBinding.Security.Mode = SecurityMode.None;
+
+            ChannelFactory<IStringReverser> revFactory = new ChannelFactory<IStringReverser>(tcpBinding, new EndpointAddress(ep));
+
+            IStringReverser revChannel = revFactory.CreateChannel();
+
+            Console.WriteLine("Connected to: " + ep);
+            while (true)
+            {
+                string str = Console.ReadLine();
+                Console.WriteLine("response: " + revChannel.ReverseString(str));
+            }
+        }
+    }
+}
---
+++ WCF_InterProgramCommunication/WCF_Server/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF_Server
+{
+    [ServiceContract]
+    public interface IStringReverser
+    {
+        [OperationContract]
+        string ReverseString(string value);
+    }
+
+    public class StringReverser : IStringReverser
+    {
+        public string ReverseString(string value)
+        {
+            char[] retVal = value.ToCharArray();
+            int idx = 0;
+            for (int i = value.Length - 1; i >= 0; i--)
+                retVal[idx++] = value[i];
+
+            string result = new string(retVal);
+            Console.WriteLine(value + " -> " + result);
+            return result;
+        }
+    }
+
+    class Program
+    {
+        public static string hostName = ConfigurationManager.AppSettings["hostName"].ToString();
+
+        static void Main(string[] args)
+        {
+            var uriString = "net.tcp://" + hostName + ":9985";
+            Console.WriteLine("Opening connection on: " + uriString);
+
+            var tcpBinding = new NetTcpBinding();
+            tcpBinding.Security.Mode = SecurityMode.None;
+
+            var uri = new Uri[] { new Uri("net.tcp://" + System.Net.Dns.GetHostName() + ":9985") };
+
+            using (var host = new ServiceHost(typeof(StringReverser), uri))
+            {
+                host.AddServiceEndpoint(typeof(IStringReverser), tcpBinding, "TcpReverse");
+
+                host.Open();
+
+                Console.WriteLine("Server is available.  Press any key to exit.");
+                Console.ReadLine();
+
+                host.Close();
+            }
+        }
+    }
+}
